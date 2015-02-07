7cb6d2f7e6aebd5b5916eef270aa192bf5e6f588
mflamer <mark@flamerassoc.com>
2015-02-06 18:11:36 -0800

    remove HDL files

--- Quartus/simulation/modelsim/rtl_work/top/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity top is
-    port(
-        clk             : in     vl_logic;
-        resetq          : in     vl_logic;
-        tail            : out    vl_logic_vector(15 downto 0)
-    );
-end top;
--- verilog/top.v
+++
@@ -1,9 +0,0 @@
-module top(
-  input clk,
-  input resetq,
-  output [15:0] tail);
-  //parameter FIRMWARE = "<firmware>";
-
-  j1 core(.clk(clk), .resetq(resetq));
-
-endmodule
